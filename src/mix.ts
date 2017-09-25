fb319ebc82069f713b95c3048ae60a8795312ddb
Julien Elbaz <julien.elbaz@web-education.net>
2017-09-25 07:55:12 +0200

    :checkered_flag: [mix] small perf improvements

--- src/mix.ts
+++ src/mix.ts
@@ -2,13 +2,15 @@ export const mix = function(one: object, two: object, mergeArrays: boolean = fal
     if(!one || !two || typeof one !== "object" || typeof two !== "object")
         return one
 
-    const clone = { ...one, ...two }
+    const clone = { ...one }
     for(const prop in two) {
         if(two.hasOwnProperty(prop)) {
             if(two[prop] instanceof Array && one[prop] instanceof Array) {
                 clone[prop] = mergeArrays ? [ ...one[prop], ...two[prop] ] : two[prop]
             } else if(typeof two[prop] === "object" && typeof one[prop] === "object") {
                 clone[prop] = mix(one[prop], two[prop], mergeArrays)
+            } else {
+                clone[prop] = two[prop]
             }
         }
     }
