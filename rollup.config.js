9655b46686b1f976cb41fff6ea9fd151f0da7fd9
elbywan <elbywan@hotmail.com>
2019-11-27 15:04:26 +0100

    :factory: Add esm bundle. #63

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,29 @@
 import typescript from "rollup-plugin-typescript"
-import { uglify } from "rollup-plugin-uglify"
+import { terser } from "rollup-plugin-terser"
 import nodeResolve from "rollup-plugin-node-resolve"
-import { minify } from "uglify-es"
 
 export default {
     input: "./src/index.umd.ts",
-    output: {
-        file: "dist/bundle/wretch.js",
-        format: "umd",
-        name: "wretch",
-        exports: "default",
-        sourcemap: true
-    },
+    output: [
+        {
+            file: "dist/bundle/wretch.js",
+            format: "umd",
+            name: "wretch",
+            exports: "default",
+            sourcemap: true
+        }
+    ],
     plugins: [
         typescript({
             typescript: require("typescript"),
             importHelpers: true
         }),
         nodeResolve(),
-        uglify({}, minify)
+        terser({
+            output: {
+                comments: false
+            }
+        })
     ],
     external: [ "url" ]
 }
